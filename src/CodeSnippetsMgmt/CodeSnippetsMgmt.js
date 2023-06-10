94e2a03c6f7a85642b761b8b708ba5d5a4a15b00
longfeiw512 <50474049+longfeiw512@users.noreply.github.com>
2023-06-10 23:18:20 +0200

    temp

---
+++ src/CodeSnippetsMgmt/CodeSnippetsMgmt.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import CodeSnippetTable from "./CodeSnippetTable";
+import CodeSnippet from "./CodeSnippet";
+
+const defaultShowingKey = -1;
+
+const CodeSnippetsMgmt = () => {
+  const [showingKey, setShowingKey] = useState(defaultShowingKey);
+
+  return showingKey === defaultShowingKey ?
+    (<CodeSnippetTable setShowingKey={setShowingKey} />) :
+    (<CodeSnippet showingKey={showingKey} setShowingKey={setShowingKey} />);
+};
+
+export default CodeSnippetsMgmt;
